ab074f243ea53e4717d8cfaa20f0e562dd46cd7b
llpw <l991788310@gmail.com>
2020-08-05 16:26:02 +0800

    drawPoint

---
+++ 01helloWebgl/helloPoint.js
@@ -0,0 +1,26 @@
+var VSHADER_SOURCE = `
+    void main() {
+        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_PointSize = 10.0;
+    }
+`;
+
+var FSHADER_SOURCE = `
+    void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+`;
+
+function main() {
+    var canvas = document.getElementById('webgl');
+    var gl = getWebGLContext(canvas);
+
+    if (!gl) return;
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) return;
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
